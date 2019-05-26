4520c741fc26f299d8160528048918c05f6d8546
MDYLL <mdylll@mail.ru>
2019-05-27 00:50:10 +0300

    relis 1

--- index2.js
+++ index2.js
@@ -1,17 +1,45 @@
+let answer;
+let question;
+let jokes=[];
+localStorage.setItem('Favorite',JSON.stringify(jokes));
+
 function loadJoke() {
-  let xhr = new XMLHttpRequest();
-  xhr.open('GET', 'https://official-joke-api.appspot.com/jokes/random', false);
-  xhr.send();
+    let xhr = new XMLHttpRequest();
+    xhr.open('GET', 'https://official-joke-api.appspot.com/jokes/random', false);
+    xhr.send();
 
-  if (xhr.status != 200) {
-    alert('Ошибка ' + xhr.status + ': ' + xhr.statusText);
-  } else {
+    if (xhr.status == 200) {
 
-    let serialObj = JSON.stringify(xhr.responseText, ['setup', 'punchline']);
-    localStorage.setItem('Favorites', 'serialObj');
-    let jsonResponse=JSON.parse(xhr.responseText);
-    alert(localStorage.getItem('Favorites'));
-    alert(jsonResponse.setup);
-    alert(jsonResponse.punchline);
-  }
+        //let serialObj = JSON.stringify(xhr.responseText, ['setup', 'punchline']);
+        //localStorage.setItem('Favorites', 'serialObj');
+        let jsonResponse = JSON.parse(xhr.responseText);
+        question = jsonResponse.setup;
+        answer = jsonResponse.punchline;
+
+        let an = document.getElementById("a");
+        an.innerText = answer;
+        let qu = document.getElementById("q");
+        qu.innerText = question;
+    } else {
+        alert('Ошибка ' + xhr.status + ': ' + xhr.statusText);
+    }
+}
+
+function addToFavorite() {
+    jokes.push({
+        q:question,
+        a:answer
+    });
+    localStorage.setItem('Favorite',JSON.stringify(jokes));
+}
+
+function showFavorite() {
+    let favorite = document.getElementById("favorite");
+    favorite.innerHTML = null;
+    let obj = JSON.parse(localStorage.getItem("Favorite"));
+    obj.forEach((item,i)=>{
+        let el1 = document.createElement('li');
+        el1.innerText = item.q+" "+item.a;
+        favorite.appendChild(el1);
+    })
 }
